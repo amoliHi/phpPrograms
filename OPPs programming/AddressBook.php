81ad66470bdfa6085c1efa1f5d4b8017874648f6
amoliHi <amolih9@gmail.com>
2019-11-25 17:11:11 +0530

    [Himanshu].Refactor.commenting.

--- OPPs programming/AddressBook.php
+++ OPPs programming/AddressBook.php
@@ -1,6 +1,9 @@
 <?php
 /**
  * Create Object Oriented Analysis and Design of a simple Address Book Problem.
+ * 
+ * This program that can be used to maintain an address book. An address book holds a collection of entries,
+ * each recording a person's first and last names, address, city, state, zip, and phone number.
  */
 
 //require functions in OPPsProgramLogic class
