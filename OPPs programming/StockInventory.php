b7f3f06073c8cc3b7cd2bef93875c6f9778b536d
amoliHi <amolih9@gmail.com>
2019-11-28 12:08:31 +0530

    [Himanshu].Refactor.json file changes

--- OPPs programming/StockInventory.php
+++ OPPs programming/StockInventory.php
@@ -3,17 +3,16 @@
 /***********************************************************************************
  * Inventory Management Program-:
  * 
- * a. Desc ­> Extend the StockReport program to Create InventoryManager to manage the
+ * a.Desc ­> Extend the StockReport program to Create InventoryManager to manage the
  * Inventory. The Inventory Manager will use InventoryFactory to create Inventory
  * Object from JSON. The InventoryManager will call each Inventory Object in its list
  * to calculate the Inventory Price and then call the Inventory Object to return the
  * JSON String. The main program will be with InventoryManager
- * b. I/P ­> read in JSON File
- * c. Logic ­> Get JSON Object in Java or NSDictionary in iOS. Create Inventory
+ * b.I/P ­> read in JSON File
+ * c.Logic ­> Get JSON Object in Java or NSDictionary in iOS. Create Inventory
  * Object from JSON. Calculate the value for every Inventory.
- * d. O/P ­> Create the JSON from Inventory Object and output the JSON String.
- * **********************************************************************************
- */
+ * d.O/P ­> Create the JSON from Inventory Object and output the JSON String.
+ ************************************************************************************/
 
 //require functions of OPPsProgramLogic class
 include "OPPsProgramLogic.php";
