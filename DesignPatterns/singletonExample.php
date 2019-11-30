d375ffb92b5ed604d52cf1cad32c852e68feb752
amoliHi <amolih9@gmail.com>
2019-11-30 12:10:24 +0530

    [Himanshu] Add . New examples added

--- DesignPatterns/singletonExample.php
+++ DesignPatterns/singletonExample.php
@@ -80,3 +80,58 @@ function clientCode()
 }
 
 clientCode();
+/*********************************************************** */
+/**
+ * Lazy initialization example
+ */
+header('Content-type:text/plain; charset=utf-8');
+
+class Fruit {
+    private $type;
+    private static $types = array();
+
+    private function __construct($type) {
+        $this->type = $type;
+    }
+
+    public static function getFruit($type) {
+        // Lazy initialization takes place here
+        if (!isset(self::types[$type])) {
+            self::types[$type] = new Fruit($type);
+        }
+
+        return self::types[$type];
+    }
+
+    public static function printCurrentTypes() {
+        echo 'Number of instances made: ' . count(self::types) . "\n";
+        foreach (array_keys(self::types) as $key) {
+            echo "$key\n";
+        }
+        echo "\n";
+    }
+}
+
+Fruit::getFruit('Apple');
+Fruit::printCurrentTypes();
+
+Fruit::getFruit('Banana');
+Fruit::printCurrentTypes();
+
+Fruit::getFruit('Apple');
+Fruit::printCurrentTypes();
+
+/*
+OUTPUT:
+
+Number of instances made: 1
+Apple
+
+Number of instances made: 2
+Apple
+Banana
+
+Number of instances made: 2
+Apple
+Banana
+*/
