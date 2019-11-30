93582e5473baab1fbc7a0c39e7ebe2a8d716cf97
amoliHi <amolih9@gmail.com>
2019-11-30 15:42:34 +0530

    [Himanshu] Refactor . TestFactory.php checked

--- DesignPatterns/FactoryDesignPattern/TestFactory.php
+++ DesignPatterns/FactoryDesignPattern/TestFactory.php
@@ -1,13 +1,8 @@
 <?php
-include "Computer"
 
-public class TestFactory {
+include "ComputerFactory.php";
 
-	public static void main(String[] args) {
-		Computer pc = ComputerFactory.getComputer("pc","2 GB","500 GB","2.4 GHz");
-		Computer server = ComputerFactory.getComputer("server","16 GB","1 TB","2.9 GHz");
-		System.out.println("Factory PC Config::"+pc);
-		System.out.println("Factory Server Config::"+server);
-	}
-
-}
+$pc = ComputerFactory::getComputer("pc","2 GB","500 GB","2.4 GHz");
+$server = ComputerFactory::getComputer("server","16 GB","1 TB","2.9 GHz");
+echo("Factory PC Config::"+$pc);
+echo("Factory Server Config::"+$server);
