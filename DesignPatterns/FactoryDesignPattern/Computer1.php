5b41928b8fdb4cd95d01537afcc7ccf3f5cc2fd5
amoliHi <amolih9@gmail.com>
2019-11-30 14:40:33 +0530

    [Himanshu] Add . Pc.php

---
+++ DesignPatterns/FactoryDesignPattern/Computer1.php
@@ -0,0 +1,13 @@
+<?php
+
+abstract class Computer {
+
+	abstract public function getRAM();
+	abstract public function getHDD();
+	abstract public function getCPU();
+	
+	//@Override
+	public function toString(){
+		return "RAM= "+this.getRAM()+", HDD="+this.getHDD()+", CPU="+this.getCPU();
+	}
+}
---
+++ DesignPatterns/FactoryDesignPattern/PC.php
@@ -0,0 +1,33 @@
+<?php
+
+class PC
+{
+	private $ram;
+	private $hdd;
+	private $cpu;
+
+	public function __construct($ram, $hdd, $cpu)
+	{
+		$this->ram = $ram;
+		$this->hdd = $hdd;
+		$this->cpu = $cpu;
+	}
+
+	//@Override
+	public function getRAM()
+	{
+		return $this->ram;
+	}
+
+	//@Override
+	public function getHDD()
+	{
+		return $this->hdd;
+	}
+
+	//@Override
+	public function getCPU()
+	{
+		return $this->cpu;
+	}
+}
