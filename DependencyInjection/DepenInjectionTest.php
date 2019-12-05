f6c4aecc914af67f066a1a31af299c884d07b824
amoliHi <amolih9@gmail.com>
2019-12-05 11:44:12 +0530

    [Himanshu] Refactor . Dependency Injection program final file

--- DependencyInjection/DepenInjectionTest.php
+++ DependencyInjection/DepenInjectionTest.php
@@ -1,10 +1,13 @@
 <?php
-
+// require functions of Product class
 include "Product.php";
 
+//@var $stockItem hold the object reference to StockItem class
 $stockItem=new StockItem(10,"Available");
+//@var $product hold the object reference to Product class
 $product=new Product(10124,$stockItem);
 echo "Product Stock status:- \n";
+//displaying product stock status
 print_r($product->getStockItem());
 echo "Product sku number:- \n";
 print_r($product->getSku());
--- DependencyInjection/StockItem.php
+++ DependencyInjection/StockItem.php
@@ -1,21 +1,46 @@
-<?php 
- 
-class StockItem {
-  
+<?php
+
+/**
+ * StockItem class have details of quantity and status of poduct
+ */
+class StockItem
+{
+
+  //@var int $quantity hold quantity of product
   private $quantity;
+  //@var string $status hold status whether product is available or not 
   private $status;
-  
-  public function __construct(int $quantity,string $status){
-   $this->quantity = $quantity;
-   $this->status   = $status;
+
+  /**
+   * Constructor function to initialize quantity and status of product
+   *
+   * @param integer $quantity
+   * @param string $status
+   * @return void
+   */
+  public function __construct(int $quantity, string $status)
+  {
+    $this->quantity = $quantity;
+    $this->status   = $status;
   }
-  
-  public function getQuantity(){
-   return $this->quantity;
+
+  /**
+   * Function to get the quantity of product
+   *
+   * @return [int] $quantity of product
+   */
+  public function getQuantity()
+  {
+    return $this->quantity;
   }
-  
-  public function getStatus(){
-   return $this->status;
+
+  /**
+   * Function to get the status(availability) of product
+   *
+   * @return [string] $status of product
+   */
+  public function getStatus()
+  {
+    return $this->status;
   }
-  
 }
